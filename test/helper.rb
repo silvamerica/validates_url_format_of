d22464b1a6fdfd07733ae4393c585bcaef9f537d
Erik Michaels-Ober <sferik@gmail.com>
2014-01-02 22:18:46 +0100

    Enforce code coverage minimums

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('..', __FILE__)
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+  minimum_coverage(100)
+end
 require 'validates_url_format_of'
 require 'active_record'
 require 'model'
